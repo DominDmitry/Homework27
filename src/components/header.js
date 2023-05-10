c988c1d943d41ae7f1e89f842d486afea35d8917
DominDmitry <demindima211090@gmail.com>
2023-05-10 21:37:42 +0300

    fin

---
+++ src/components/header.js
@@ -0,0 +1,9 @@
+import './header.css';
+const Header = () =>{
+    return (
+    <header>
+      <p>some Header Text</p>
+    </header>
+    )
+}
+export default Header
---
+++ src/components/sidebar.js
@@ -0,0 +1,17 @@
+import './sidebar.css';
+const Sidebar = () =>{
+    return (
+        <aside className="sidebar">
+        <nav>
+          <ul>
+            <li><a href="/service-1/">Услуга 1</a></li>
+            <li><a href="/service-2/">Услуга 2</a></li>
+            <li><a href="/service-3/">Услуга 3</a></li>
+            <li><a href="/service-3/">Услуга 4</a></li>
+            <li><a href="/service-3/">Услуга 5</a></li>
+          </ul>
+        </nav>
+      </aside>
+    )
+}
+export default Sidebar
